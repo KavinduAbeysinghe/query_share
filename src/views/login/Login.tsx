5bc1463af85433257e682cef2a2481dcc6d8aef2
KavinduAbeysinghe <kavinduabeysinghe2001@gmail.com>
2023-07-29 18:35:04 +0530

    modified:   src/views/login/Login.tsx

--- src/views/login/Login.tsx
+++ src/views/login/Login.tsx
@@ -4,6 +4,7 @@ import {
   IconButton,
   InputAdornment,
   Typography,
+  useTheme,
 } from "@mui/material";
 import CustomInputField from "../../components/CustomInputField";
 import { useForm } from "react-hook-form";
@@ -17,6 +18,7 @@ import { faEye, faEyeSlash } from "@fortawesome/free-regular-svg-icons";
 import logo from "../../assets/images/logo.png";
 
 const Login = () => {
+  const theme = useTheme();
   const commonError = "Field is required";
 
   const validationSchema = yup.object().shape({
@@ -103,95 +105,113 @@ const Login = () => {
             </Typography>
           </Box>
         </Grid>
-        <Grid item xs={12} sm={12} md={6} lg={6} xl={6} sx={{ p: 5 }}>
-          <Typography variant={"h4"} fontWeight={700} textAlign={"center"}>
-            Welcome Back
-          </Typography>
-          <Typography variant={"h5"} fontWeight={400} sx={{ mt: 3 }}>
-            Sign In to continue
-          </Typography>
-          <form className="form-container">
-            <Grid container flexDirection={"column"}>
-              <Grid item mt={5}>
-                <CustomInputField
-                  label={"Email"}
-                  register={register("email")}
-                  errors={!!errors?.email?.message}
-                  helperText={errors?.email?.message?.toString()}
-                  disabled={false}
-                  id={"email"}
-                  placeholder={"someone@example.com"}
-                />
-              </Grid>
-              <Grid item mt={3}>
-                <CustomInputField
-                  label={"Password"}
-                  register={register("password")}
-                  errors={!!errors?.password?.message}
-                  helperText={errors?.password?.message?.toString()}
-                  disabled={false}
-                  id={"password"}
-                  placeholder={"Your password here"}
-                  type={showPassword ? "text" : "password"}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton
-                          aria-label="toggle password visibility"
-                          onClick={handleClickShowPassword}
-                          onMouseDown={handleMouseDownPassword}
-                          edge="end"
-                        >
-                          {showPassword ? (
-                            <FontAwesomeIcon icon={faEye} color="#C1C1C1" />
-                          ) : (
-                            <FontAwesomeIcon
-                              icon={faEyeSlash}
-                              color="#C1C1C1"
-                            />
-                          )}
-                        </IconButton>
-                      </InputAdornment>
-                    ),
-                  }}
-                />
-                <Typography
-                  textAlign={"end"}
-                  color={"#4253F0"}
-                  onClick={handleForgotPassword}
-                  sx={{ cursor: "pointer" }}
-                >
-                  Forgot Password?
-                </Typography>
-              </Grid>
-              <Grid item mt={5}>
-                <PrimaryButton
-                  type={"submit"}
-                  buttonText={"LOGIN"}
-                  onClick={handleSubmit(onSubmit)}
-                  variant="contained"
-                />
-              </Grid>
-              <Grid item mt={3}>
-                <PrimaryButton
-                  buttonText="Sign In with Google"
-                  variant="outlined"
-                  startIcon={<Google />}
-                />
-              </Grid>
-              <Grid item textAlign={"center"} mt={3}>
-                <Typography sx={{ color: "gray" }}>
-                  Don't have an acoount?{" "}
-                  <span
-                    style={{ color: "#4253F0", cursor: "pointer" }}
-                    onClick={handleSignUp}
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={6}
+          lg={6}
+          xl={6}
+          sx={{
+            [theme.breakpoints.up("sm")]: { p: theme.spacing(0) },
+            [theme.breakpoints.up("md")]: { p: theme.spacing(5) },
+          }}
+        >
+          <Box
+            sx={{
+              margin: "auto",
+              maxWidth: "500px",
+            }}
+          >
+            <Typography variant={"h4"} fontWeight={700} textAlign={"center"}>
+              Welcome Back
+            </Typography>
+            <Typography variant={"h5"} fontWeight={400} sx={{ mt: 3 }}>
+              Sign In to continue
+            </Typography>
+            <form className="form-container">
+              <Grid container flexDirection={"column"}>
+                <Grid item mt={5}>
+                  <CustomInputField
+                    label={"Email"}
+                    register={register("email")}
+                    errors={!!errors?.email?.message}
+                    helperText={errors?.email?.message?.toString()}
+                    disabled={false}
+                    id={"email"}
+                    placeholder={"someone@example.com"}
+                  />
+                </Grid>
+                <Grid item mt={3}>
+                  <CustomInputField
+                    label={"Password"}
+                    register={register("password")}
+                    errors={!!errors?.password?.message}
+                    helperText={errors?.password?.message?.toString()}
+                    disabled={false}
+                    id={"password"}
+                    placeholder={"Your password here"}
+                    type={showPassword ? "text" : "password"}
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                            edge="end"
+                          >
+                            {showPassword ? (
+                              <FontAwesomeIcon icon={faEye} color="#C1C1C1" />
+                            ) : (
+                              <FontAwesomeIcon
+                                icon={faEyeSlash}
+                                color="#C1C1C1"
+                              />
+                            )}
+                          </IconButton>
+                        </InputAdornment>
+                      ),
+                    }}
+                  />
+                  <Typography
+                    textAlign={"end"}
+                    color={"#4253F0"}
+                    onClick={handleForgotPassword}
+                    sx={{ cursor: "pointer" }}
                   >
-                    Sign Up Now!
-                  </span>
-                </Typography>
+                    Forgot Password?
+                  </Typography>
+                </Grid>
+                <Grid item mt={5}>
+                  <PrimaryButton
+                    type={"submit"}
+                    buttonText={"LOGIN"}
+                    onClick={handleSubmit(onSubmit)}
+                    variant="contained"
+                  />
+                </Grid>
+                <Grid item mt={3}>
+                  <PrimaryButton
+                    buttonText="Sign In with Google"
+                    variant="outlined"
+                    startIcon={<Google />}
+                  />
+                </Grid>
+                <Grid item textAlign={"center"} mt={3}>
+                  <Typography sx={{ color: "gray" }}>
+                    Don't have an acoount?{" "}
+                    <span
+                      style={{ color: "#4253F0", cursor: "pointer" }}
+                      onClick={handleSignUp}
+                    >
+                      Sign Up Now!
+                    </span>
+                  </Typography>
+                </Grid>
               </Grid>
-            </Grid>
-          </form>
+            </form>
+          </Box>
         </Grid>
       </Grid>
     </Box>
